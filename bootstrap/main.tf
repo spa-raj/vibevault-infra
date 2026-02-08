c60cefa0570235a7845e16133ab4f00cfbdc0b24
Sparsh <sparsh.raj30@gmail.com>
2026-02-08 19:19:28 +0530

    Remove unused bucket_key_enabled flag from S3 encryption config since it only applies to SSE-KMS
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -49,7 +49,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
-    bucket_key_enabled = true
   }
 }
 
