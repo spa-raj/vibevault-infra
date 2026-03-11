1a0addff2f678351985fa563229e1adddb86e75e
Sparsh <sparsh.raj30@gmail.com>
2026-03-11 22:33:21 +0530

    Enable force_delete on ECR repositories for clean teardown
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -14,6 +14,7 @@ resource "aws_ecr_repository" "this" {
 
   name                 = "${var.project_name}-${var.environment}/${each.value}"
   image_tag_mutability = var.image_tag_mutability
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
